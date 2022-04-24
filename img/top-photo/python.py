d029ab964fd3614357bbfcbb9f35a30e703bf455
timebusker <timebusker@qq.com>
2022-04-24 21:05:49 +0800

    2022/04/24 21:05:48 ____ :sun_with_face::sunflower::palm_tree::house_with_garden::office::octocat::guitar::meat_on_bone:

---
+++ img/top-photo/python.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Author: HUAWEI
+# @Date:   2022-04-24 20:37:53
+# @Last Modified by:   HUAWEI
+# @Last Modified time: 2022-04-24 20:52:54
+
+import os
+import random
+path='Z:\\timebusker\\timebusker.github.io\\img\\top-photo'       
+#获取该目录下所有文件，存入列表中
+lists=os.listdir(path)
+for file in lists:
+    if file.endswith('.jpg'):
+        print(file)
+        old=path+ os.sep + file
+        new=path+ os.sep + str(random.randint(0,999)) + file
+        os.rename(old,new)
+
+lists=os.listdir(path)
+n=1
+for file in lists:
+    if file.endswith('.jpg'):
+        print(file)
+        old=path+ os.sep + file
+        new=path+ os.sep + str(n) + '.jpg'
+        os.rename(old,new)
+        n+=1
