ada59961ad187d21a643bdcd3ad6bc1cbd27d3c4
timebusker <timebusker@qq.com>
2024-04-06 22:48:02 +0800

    2024/04/06 22:48:02 ____ :sun_with_face::sunflower::palm_tree::house_with_garden::office::octocat::guitar::meat_on_bone:

--- img/top-photo/0_rename.bat
+++ img/top-photo/0_rename.bat
@@ -2,7 +2,11 @@
 setlocal enabledelayedexpansion
 set /a count=1
 for %%i in (*.jpg) do (
-    set "filename=!count!"
+    :: 带0补位
+    :: set "filename=000!count!"
+    :: ren "%%i" "!filename~-3!.jpg"
+	:: 不带0补位
+	set "filename=!count!"
     ren "%%i" "!filename!.jpg"
     set /a count+=1
 )
