530f3e91a3cec35f946dd3124dd87b2b769b7700
timebusker <timebusker@gmail.com>
2025-01-21 17:56:20 +0800

    2025/01/21 17:56:20 __ submit_blog_document

--- settings/windows/bat/一键安装mvn_jar.bat
+++
@@ -1,5 +0,0 @@
-::mvn install:install-file -Dfile=jar包的位置 -DgroupId=上面的groupId -DartifactId=上面的artifactId -Dversion=上面的version -Dpackaging=jar
-
-mvn install:install-file -Dfile=D:/Workspaces/Eclipse-mars/sample-pay/sample-pay/sample-pay-Alipay/src/main/webapp/WEB-INF/lib/alipay-sdk-java20170307171631.jar -DgroupId=alipay -DartifactId=alipay-sdk-java20170307171631 -Dversion=1.0 -Dpackaging=jar
-
-::mvn install:install-file -Dfile=D:/Workspaces/Eclipse-mars/roncoocom/roncoo-pay/roncoo-pay-service/src/lib/alipay-trade-sdk.jar -DgroupId=alipay -DartifactId=alipay-trade-sdk -Dversion=1.0 -Dpackaging=jar
--- settings/windows/bat/一键设置鼠标右键CMD.bat
+++
@@ -1,3 +0,0 @@
-REG ADD "HKCR\*\shell\Cmd\command" /ve /t REG_EXPAND_SZ /d %ComSpec%  
-REG ADD "HKCR\Directory\shell\Cmd\command" /ve /t REG_EXPAND_SZ /d "%ComSpec% /k cd %1"  
-REG ADD "HKCR\Drive\shell\Cmd\command" /ve /t REG_EXPAND_SZ /d "%ComSpec% /k cd %1"  
