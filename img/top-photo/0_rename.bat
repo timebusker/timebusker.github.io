4830046bebd8cc4dc8f6817cbd31afddb9b3b17a
timebusker <timebusker@gmail.com>
2025-10-22 00:05:23 +0800

    2025/10/22  0:05:22 __ submit_blog_document

--- img/top-photo/0_rename.bat
+++ img/top-photo/0_rename.bat
@@ -2,12 +2,11 @@
 setlocal enabledelayedexpansion
 set /a count=1
 for %%i in (*.jpg) do (
+    echo %%i 
     :: 带0补位
-    :: set "filename=000!count!"
-    :: ren "%%i" "!filename~-3!.jpg"
-	:: 不带0补位
-	set "filename=!count!"
-    ren "%%i" "!filename!.jpg"
+    :: ren "%%i" "0!count!.jpg"
+    :: 不带0补位
+    ren "%%i" "!count!.jpg"
     set /a count+=1
 )
 pause
